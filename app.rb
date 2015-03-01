df240f74c9c678b9adebbc1771bd1eeebae02647
Anh Nguyen <anhpnguyen.an@gmail.com>
2015-03-01 03:00:09 -0600

    fix bugs

--- app.rb
+++ app.rb
@@ -5,8 +5,7 @@ require 'date'
 
 class DisplayHolidays < Sinatra::Base
   get '/' do
-    @holidays = HolidApi.get(country: 'us', year: Time.now.year, month: Time.now.month)
-    @birth = HolidApi.get(country: 'us', year: 1994, month: 3)
+    @holidays = HolidApi.get(params)
     erb :index
   end
 end
